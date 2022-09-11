56a06ad827e18029908380f12f2feba2991f6542
rafathasan <rafathasankhan@gmail.com>
2022-09-11 20:14:14 +0600

    update key listerner

--- main.py
+++ main.py
@@ -30,56 +30,52 @@ driver = webdriver.Chrome(driver_path)
 
 # In[ ]:
 
+def search_on_scholar(driver):
+    query = driver.execute_script("return window.getSelection().toString()")
+    driver.get(search_scholar.format(query))
+
+def update_department(driver):
+    department = driver.execute_script("return window.getSelection().toString()")
+    df.loc[i, ["department"]] = department.strip()
+
+def update_name(driver):
+    name = driver.execute_script("return window.getSelection().toString()")
+    df.loc[i, ["name"]] = name.strip()
+
+def update_email(driver):
+    email = driver.execute_script("return window.getSelection().toString()")
+    df.loc[i, ["email"]] = email.strip()
+
+def update_topic(driver):
+    topic = driver.execute_script("return window.getSelection().toString()")
+    df.loc[i, ["topic"]] = topic.strip()
+
+init_handle = driver.current_window_handle
 
 for i in range(len(df)):
+    # Search University
+    topic = df.loc[i, ["topic"]].item()
+    print(topic)
+    if topic != "":
+        continue
     query = df.loc[i, ["university"]].item()
     driver.get(search_university.format(query))
 
-    while True:
-        if keyboard.is_pressed("spacebar"):
-            break
-    name = driver.execute_script("return window.getSelection().toString()")
-    df.loc[i, ["name"]] = name
+    keyboard.read_key()
+    keyboard.add_hotkey("z", search_on_scholar, args=(driver,))
+    keyboard.add_hotkey("a", update_department, args=(driver,))
+    keyboard.add_hotkey("s", update_name, args=(driver,))
+    keyboard.add_hotkey("d", update_email, args=(driver,))
+    keyboard.add_hotkey("f", update_topic, args=(driver,))
 
-    while True:
-        if keyboard.is_pressed("spacebar"):
-            break
-    email = driver.execute_script("return window.getSelection().toString()")
-    df.loc[i, ["email"]] = email
+    keyboard.wait("esc")
 
+    for handle in driver.window_handles:
+        driver.switch_to.window(handle)
+        if handle != init_handle:
+            driver.close()
 
-# In[ ]:
-
-
-driver.get(search_scholar.format("W Li"))
+    driver.switch_to.window(init_handle)
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+    df.to_csv(data_path, index=False)
